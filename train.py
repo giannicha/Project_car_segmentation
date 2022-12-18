c0c1dc39c2447250f2bb86c9122f214a079e10bc
imlim <imlim0813@hanyang.ac.kr>
2022-12-19 04:52:40 +0900

    copy train.py

---
+++ train.py
@@ -0,0 +1,62 @@
+import argparse
+import collections
+import torch
+import numpy as np
+import data_loader.data_loaders as module_data
+import model.loss as module_loss
+import model.metric as module_metric
+import model.model as module_arch
+from parse_config import ConfigParser
+# from trainer import Trainer
+
+
+# 랜덤 시드를 고정함으로써 동일한 조건으로 시작.
+SEED = 123
+torch.manual_seed(SEED)
+torch.backends.cudnn.deterministic = True
+torch.backends.cudnn.benchmark = False
+np.random.seed(SEED)
+
+
+def main():
+
+    config = ConfigParser(config='./config.json', run_id='ver_0')
+
+    # 1.
+    data_loader = getattr(module_data, 'CustomDataset')()
+    valid_data_loader = data_loader.split_validation()
+
+    # build model architecture, then print to console
+    model = config.init_obj('arch', module_arch)
+
+    # prepare for (multi-device) GPU training
+    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+    model = model.to(device)
+    if len(device_ids) > 1:
+        model = torch.nn.DataParallel(model, device_ids=device_ids)
+
+    # get function handles of loss and metrics
+    criterion = getattr(module_loss, config['loss'])
+    metrics = [getattr(module_metric, met) for met in config['metrics']]
+
+    # build optimizer, learning rate scheduler. delete every lines containing lr_scheduler for disabling scheduler
+    trainable_params = filter(lambda p: p.requires_grad, model.parameters())
+    optimizer = config.init_obj('optimizer', torch.optim, trainable_params)
+    lr_scheduler = config.init_obj('lr_scheduler', torch.optim.lr_scheduler, optimizer)
+
+    trainer = Trainer(model, criterion, metrics, optimizer,
+                      config=config,
+                      device=device,
+                      data_loader=data_loader,
+                      valid_data_loader=valid_data_loader,
+                      lr_scheduler=lr_scheduler)
+
+    trainer.train()
+
+
+if __name__ == '__main__':
+    args = argparse.ArgumentParser(description='PyTorch Template')
+    args.add_argument('-c', '--config', default=None, type=str,
+                      help='config file path (default: None)')
+
+    print(args)
