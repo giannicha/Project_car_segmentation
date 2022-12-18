7f6bad6247baf0769b7036365773714d6607af8d
imLim <imlim0813@hanyang.ac.kr>
2022-12-18 23:09:09 +0900

    preprocess using Albumentations

--- data_loader/data_loaders.py
+++ data_loader/data_loaders.py
@@ -1,11 +1,14 @@
 import torch
 import glob
+import numpy as np
+import albumentations as A
 
 from PIL import Image
 from typing import Union, Tuple, List
 from pathlib import Path
 from torch.utils.data import DataLoader
 from torchvision.transforms import transforms
+from albumentations.pytorch import ToTensorV2
 
 
 class CustomDataset(torch.utils.data.Dataset):
@@ -15,10 +18,10 @@ class CustomDataset(torch.utils.data.Dataset):
     :param img_path: 이미지 데이터가 포함된 리스트
     :param mask_path: 라벨 데이터가 포함된 리스트
     :param train: 학습 데이터 : True / 평가 데이터 : False
-    :param train_transform: 전처리 객체
+    :param transform: 전처리 객체
     """
     def __init__(self, img_path: List, mask_path: List, img_size: Tuple = (224, 224),
-                 train: bool = True, train_transform: transforms.Compose = None):
+                 train: bool = True, transform: transforms.Compose = None):
 
         # 이미지 경로 설정
         self.img_data = img_path
@@ -31,19 +34,10 @@ class CustomDataset(torch.utils.data.Dataset):
         # 학습 데이터 인덱스 = (전체 이미지 개수 * 학습 데이터 비율) / 폴더 개수
         train_idx = int(len(self.img_data) * split_ratio / folder_num)
 
-        # 전처리 객체
-        # 하이퍼 파라미터
-        # 1. img_size : 학습에 사용될 이미지 크기
-        self.img_size = img_size
-        self.transform = transforms.Compose([
-            transforms.Resize(size=self.img_size),
-            transforms.ToTensor()
-        ])
+        # 전처리
+        self.transform = transform
 
         if train:
-            # 학습 데이터라면 전처리 객체를 변경.
-            self.transform = train_transform
-
             # Case 1. 학습 데이터
             # 각 폴더별로 1000개씩 데이터가 존재하는 것을 확인. -> train_idx = 800
             # 각 폴더로부터 학습 데이터 인덱스만큼 데이터를 가져온다.
@@ -76,16 +70,13 @@ class CustomDataset(torch.utils.data.Dataset):
         :param idx: 인덱스
         :return: img, label
         """
-        img = self.img_data[idx]
-        img = Image.open(img)
-        img = self.transform(img)
+        img = np.array(Image.open(self.img_data[idx]))
+        label = np.array(Image.open(self.mask_data[idx]))
 
-        label = self.mask_data[idx]
-        label = Image.open(label)
-
-        # 세그멘테이션 데이터는 크기를 제외하고는 전처리를 하지 않는다.
-        mask_transform = transforms.Compose([transforms.Resize(size=self.img_size), transforms.ToTensor()])
-        label = mask_transform(label)
+        aug = self.transform(image=img, mask=label)
+        img = aug['image']
+        label = aug['mask']
+        label = torch.max(label, dim=2)[0]
 
         return img, label
 
@@ -94,15 +85,17 @@ if __name__ == '__main__':
     # Config.json에 들어가야 할 것들 : img_size, mean, std
     # train.py에 들어가야 할 것들 : img_path, mask_path, train_transform, CustomDataset
 
-    img_path = sum([glob.glob(f'./Data/data{i}/data{i}/CameraRGB/*') for i in ['A', 'B', 'C', 'D', 'E']], [])
-    mask_path = sum([glob.glob(f'./Data/data{i}/data{i}/CameraSeg/*') for i in ['A', 'B', 'C', 'D', 'E']], [])
-    img_size = (224, 224)
-    mean = (0.5, 0.5, 0.5)
-    std = (0.5, 0.5, 0.5)
-    train_transform = transforms.Compose([transforms.Resize(img_size),
-                                          transforms.ToTensor(),
-                                          transforms.Normalize(mean, std)])
+    img_path = sum([glob.glob(f'../Data/data{i}/data{i}/CameraRGB/*') for i in ['A', 'B', 'C', 'D', 'E']], [])
+    mask_path = sum([glob.glob(f'../Data/data{i}/data{i}/CameraSeg/*') for i in ['A', 'B', 'C', 'D', 'E']], [])
+    img_size = (160, 240)
+    t1 = A.Compose([
+        A.Resize(img_size[0], img_size[1]),
+        A.augmentations.transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
+        ToTensorV2()
+    ])
 
     train_data = CustomDataset(img_path=img_path, mask_path=mask_path, img_size=img_size, train=True,
-                               train_transform=train_transform)
-    test_data = CustomDataset(img_path=img_path, mask_path=mask_path, img_size=img_size, train=False)
+                               transform=t1)
+    test_data = CustomDataset(img_path=img_path, mask_path=mask_path, img_size=img_size, train=False,
+                              transform=t1)
+    print()
