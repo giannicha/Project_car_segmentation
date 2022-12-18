5be7406b7e455094a25425ef3b848e53ff562125
imLim <imlim0813@hanyang.ac.kr>
2022-12-19 00:18:26 +0900

    create base model

---
+++ model/model.py
@@ -0,0 +1,14 @@
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+
+
+class Custom_Model(nn.Module):
+    """
+    세그멘테이션 모델을 정의하는 클래스
+    """
+    def __init__(self):
+        pass
+
+    def forward(self, x):
+        pass
