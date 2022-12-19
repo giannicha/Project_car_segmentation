b972d9435094bf2a903240ac7a14785da2222772
imlim <imlim0813@hanyang.ac.kr>
2022-12-19 10:12:12 +0900

    Create train loop function

--- train.py
+++ train.py
@@ -1,5 +1,5 @@
 import argparse
-import collections
+import albumentations as A
 import torch
 import numpy as np
 import data_loader.data_loaders as module_data
@@ -8,55 +8,97 @@ import model.metric as module_metric
 import model.model as module_arch
 from parse_config import ConfigParser
 # from trainer import Trainer
+from data_loader.data_loaders import make_dataloder
+from albumentations.pytorch import ToTensorV2
 
 
 # 랜덤 시드를 고정함으로써 동일한 조건으로 시작.
-SEED = 123
+SEED = 42
 torch.manual_seed(SEED)
 torch.backends.cudnn.deterministic = True
 torch.backends.cudnn.benchmark = False
 np.random.seed(SEED)
 
 
-def main():
+def main(config_):
 
-    config = ConfigParser(config='./config.json', run_id='ver_0')
+    config = ConfigParser(config=config_, run_id='ver_0')
+    train_config = {}
 
-    # 1.
-    data_loader = getattr(module_data, 'CustomDataset')()
-    valid_data_loader = data_loader.split_validation()
+    # 1. 데이터 로더 생성
+    loader_dict = dict(config.data_loader['args'])
 
-    # build model architecture, then print to console
-    model = config.init_obj('arch', module_arch)
+    # Train / Test 전처리 객체 생성
+    train_transform = A.Compose([
+        A.Resize(loader_dict['img_size'][0], loader_dict['img_size'][1]),
+        A.augmentations.transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
+        ToTensorV2()
+    ])
 
-    # prepare for (multi-device) GPU training
-    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-    model = model.to(device)
-    if len(device_ids) > 1:
-        model = torch.nn.DataParallel(model, device_ids=device_ids)
+    test_transform = A.Compose([
+        A.Resize(loader_dict['img_size'][0], loader_dict['img_size'][1]),
+        A.augmentations.transforms.Normalize(mean=(0.5, 0.5, 0.5), std=(0.5, 0.5, 0.5)),
+        ToTensorV2()
+    ])
 
-    # get function handles of loss and metrics
-    criterion = getattr(module_loss, config['loss'])
-    metrics = [getattr(module_metric, met) for met in config['metrics']]
+    train_config['Image size'] = loader_dict['img_size']
 
-    # build optimizer, learning rate scheduler. delete every lines containing lr_scheduler for disabling scheduler
-    trainable_params = filter(lambda p: p.requires_grad, model.parameters())
-    optimizer = config.init_obj('optimizer', torch.optim, trainable_params)
-    lr_scheduler = config.init_obj('lr_scheduler', torch.optim.lr_scheduler, optimizer)
+    # 배치 사이즈를 리스트로 받아 리스트 원소 개수만큼 반복
+    for batch_size in loader_dict['batch_size']:
+        train_config['Batch size'] = batch_size
 
-    trainer = Trainer(model, criterion, metrics, optimizer,
-                      config=config,
-                      device=device,
-                      data_loader=data_loader,
-                      valid_data_loader=valid_data_loader,
-                      lr_scheduler=lr_scheduler)
+        # 배치사이즈에 따른 데이터 로더 생성
+        train_dataloader = make_dataloder(data_dir=loader_dict['data_dir'], transform=train_transform,
+                                          train_=True, batch_size=batch_size)
 
-    trainer.train()
+        valid_dataloader = make_dataloder(data_dir=loader_dict['data_dir'], transform=test_transform,
+                                          train_=False, batch_size=batch_size)
+
+        # Learning rate를 리스트로 받아 리스트 원소 개수만큼 반복
+        for learning_rate in config.optimizer['args']['lr']:
+            train_config['Initial LR'] = learning_rate
+
+            # 모델 생성
+            model = getattr(module_arch, config.module_name)()
+
+            # GPU 설정
+            device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
+            model = model.to(device)
+            train_config['GPU'] = True if torch.cuda.is_available() else 'False'
+
+            # 손실 함수, 평가지표 함수 생성
+            criterion = getattr(module_loss, config.loss_fn)
+            metrics = [getattr(module_metric, met) for met in config.metric_fn]
+            train_config['Loss function'] = config.loss_fn
+            train_config['Metric'] = [metric for metric in config.metric_fn]
+
+            # 옵티마이저 생성 및 learning rate scheduler 생성
+            trainable_params = filter(lambda p: p.requires_grad, model.parameters())
+            optimizer = getattr(torch.optim, config.optimizer['type'])(trainable_params, lr=learning_rate,
+                                                                       weight_decay=config.optimizer['args']['weight_decay'])
+            lr_scheduler = getattr(torch.optim.lr_scheduler, config.lr_scheduler['type'])(optimizer=optimizer,
+                                                                                          gamma=config.lr_scheduler['args']['gamma'],
+                                                                                          step_size=config.lr_scheduler['args']['step_size'])
+            train_config['Optimizer'] = {'Name': config.optimizer['type'], 'Weight decay': config.optimizer['args']['weight_decay']}
+            train_config['LR scheduler'] = {'Name': config.lr_scheduler['type'], 'gamma': config.lr_scheduler['args']['gamma'],
+                                            'Step size': config.lr_scheduler['args']['weight_decay']}
+
+            for epoch in config.trainer['epochs']:
+                train_config['Epoch'] = epoch
+
+                trainer = Trainer(model, criterion, metrics, optimizer,
+                                  config=config,
+                                  device=device,
+                                  data_loader=train_dataloader,
+                                  valid_data_loader=valid_dataloader,
+                                  lr_scheduler=lr_scheduler)
+                trainer.train()
 
 
 if __name__ == '__main__':
-    args = argparse.ArgumentParser(description='PyTorch Template')
+    args = argparse.ArgumentParser(description='Segmentation Template')
     args.add_argument('-c', '--config', default=None, type=str,
                       help='config file path (default: None)')
+    parser = args.parse_args()
 
-    print(args)
+    main(parser.config)
